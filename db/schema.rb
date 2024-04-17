8eb069ae2abf33edcc15d6e5306620853ee81bb5
alexisxaltamirano <alexisaltamirano1998@gmail.com>
2024-04-17 12:00:41 -0700

    added controller and updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_17_174903) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_17_185959) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,10 +34,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_17_174903) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password_digest"
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password"
   end
 
 end
